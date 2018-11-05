019fb90c170782fc0015bf6e293aeda2bebc5f78
傅兵 <fubing90208@gmail.com>
2018-11-06 00:13:24 +0800

    主页，引入图标

---
+++ src/components/TopBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="top-bar-view">
+    <i class="iconfont arrow">&#xe600;</i>
+    <span>阜宁县不动产移动端</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TopBar'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .top-bar-view
+    width: 100%
+    height: 1.12rem
+    line-height: 1.12rem
+    position: fixed
+    left: 0
+    top: 0
+    text-align: center
+    font-size: 20px
+    background-image: linear-gradient(-225deg, #3B7BFF 0%, #29AEFF 100%)
+    color: #fff
+    .arrow
+      width: .8rem
+      height: .8rem
+      line-height: .8rem
+      position: absolute
+      top: 50%
+      left: .34rem
+      transform: translate(0, -50%)
+</style>
+
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,58 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TopBar />
+    <div class="home-item bdc">
+      <span class="title">不动产业务</span>
+      <span class="text"><i class="count-icon"></i>条待审批</span>
+    </div>
+    <div class="home-item zhsw">
+      <span class="title">综合事务</span>
+      <span class="text"><i class="count-icon"></i>条待审批</span>
+    </div>
+    <div class="home-item qclc">
+      <span class="title">起草流程</span>
+      <span class="text"><i class="count-icon"></i>条待审批</span>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+// import HelloWorld from '@/components/HelloWorld.vue'
+import TopBar from '@/components/TopBar';
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    TopBar
   }
 }
 </script>
+
+<style lang="stylus" scoped>
+  .home
+    padding: 1.48rem .4rem .9rem
+    .home-item
+      width: 100%
+      height: 1.36rem
+      line-height: 1.36rem
+      display: flex
+      justify-content: space-between
+      box-sizing: border-box
+      padding: 0 .32rem
+      margin: .3rem 0  
+      box-shadow: 0 0 .04rem 0 rgba(255,155,155,0.50)
+      border-radius: .1rem
+      color: #fff
+      &.bdc
+        background-image: linear-gradient(-269deg, #FF8A9B 0%, #FEACA1 100%)
+      &.zhsw
+        background-image: linear-gradient(-269deg, #0CC1EB 0%, #16ECB1 100%)
+      &.qclc
+        background-image: linear-gradient(-270deg, #9095FE 0%, #ED98F2 100%)
+      .title 
+        width: 1.6rem
+      .text
+        flex: 1
+        text-align: right
+</style>
+
